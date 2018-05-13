cb7acfdd54272de2d1719018d4f65d159c6ed123
Mykola <chmutkola@gmail.com>
2018-05-13 13:31:21 +0300

    add login service

--- src/app/pages/login/login.module.ts
+++ src/app/pages/login/login.module.ts
@@ -1,17 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MaterialModule } from '../../material/material.module';
+import { ParticlesModule } from 'angular-particle';
+import { MobxAngularModule } from 'mobx-angular';
+
+import { LoginService } from './services/login-services/login.service';
 
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginPageComponent } from './components/login-page/login-page.component';
-
-import { ReactiveFormsModule } from '@angular/forms'
-
-import { MaterialModule } from '../../material/material.module'
-import { ParticlesModule } from 'angular-particle';
 import { LogInComponent } from './components/log-in/log-in.component';
 import { SignUpComponent } from './components/sign-up/sign-up.component';
-import { MobxAngularModule } from 'mobx-angular';
+
 
 @NgModule({
   imports: [
@@ -22,6 +23,7 @@ import { MobxAngularModule } from 'mobx-angular';
     LoginRoutingModule,
     HttpClientModule,
     MobxAngularModule,
+    LoginService,
   ],
   declarations: [
     LoginPageComponent,
